645cb7913fe875c1a855bd0ad0762e19590c11d6
Gwonyi Moon <102517739+M00NPANG@users.noreply.github.com>
2022-05-17 00:08:18 +0900

    BOJ 2839 설탕 배달

---
+++ 02_Menti-Practice/M00NPANG-Practice/src/BOJ/BOJ_2839.java
@@ -0,0 +1,23 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ_2839 {
+
+	public static void main(String[] args) {
+		Scanner scanf = new Scanner(System.in);
+		int N = scanf.nextInt();
+		
+		if (N == 4 || N == 7)
+			System.out.println(-1);
+		
+		else if (N % 5 == 0)
+			System.out.println(N / 5);
+		
+		else if(N % 5 == 1 || N % 5 == 3)
+			System.out.println((N / 5) + 1);
+		
+		else if (N % 5 == 2 || N % 5 == 4)
+			System.out.println((N / 5)+2);
+		}
+	}
