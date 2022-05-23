13a4510dd31217de46b5b1564b8de0aa6a5b5ae9
Gwonyi Moon <102517739+M00NPANG@users.noreply.github.com>
2022-05-23 15:00:12 +0900

    프로그래머스 68935 3진법뒤집기

---
+++ 02_Menti-Practice/M00NPANG-Practice/src/programmers/PG_68935.java
@@ -0,0 +1,35 @@
+package programmers;
+
+import java.util.*;
+
+public class PG_68935 {
+
+	public static void main(String[] args) {
+		Scanner scanf = new Scanner(System.in);
+		int N = scanf.nextInt();
+		int answer = 0;
+		
+		List<Integer> num =new ArrayList<>();
+		 
+		while(N != 0) {
+			 num.add(N%3);
+			 N = N/3;
+		}
+		
+		for (int i = 0; i < num.size(); i++) {
+			answer += power(num.get(i), (num.size()-i));
+		}
+		System.out.println(answer);
+	}
+	
+	static int power(int n , int pv) {
+		int result = n;
+		
+		if (pv == 1)return n;
+		for (int i = pv ; i > 1; i--) {
+			result *= 3;
+		}return result;
+	}
+	
+
+}
